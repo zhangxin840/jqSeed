cbc658906b75918247f163dea21eb308bab7ba14
Zhang Xin <zhangxin840@gmail.com>
2013-07-14 22:22:41 +0800

    First commit

---
+++ jqSeed.js
@@ -0,0 +1,58 @@
+/* Author: Zhang Xin */
+
+/* For jsHint */
+/* global jQuery: false*/
+/* jshint devel: true */
+
+;(function($) {"use strict";
+    var pluginName = 'restoreForm';
+
+    var pluginController = function(element, theOptions) {
+        var $container = $(element);
+        var defaults = {
+
+        };
+
+        // Get options saved within $container's data attributes
+        var meta = $container.data(pluginName + '-options');
+        var options = $.extend(defaults, meta, theOptions);
+
+        var init = function() {
+
+        };
+
+        // Destroy the warp object without removing elements
+        var destroy = function() {
+            // Unbind events
+            $container.off('.' + pluginName);
+            $container.find('*').off('.' + pluginName);
+            // Remove data
+            $container.removeData(pluginName);
+            $container = null;
+        };
+
+        // Wapper object
+        var that = {};
+        that.options = options;
+        that.destroy = destroy;
+
+        // Initialize the wrapper object to generate elements of the widget
+        init();
+
+        //
+        // Store wrapper object in $container using jQuery's $.data function.
+        // Usage: console.log($('#theListener').data('jqListener'));
+        $container.data(pluginName, that);
+    };
+
+    //
+    // jQuery function
+    //
+    $.fn[pluginName] = function(options) {
+        this.each(function() {
+            pluginController(this, options);
+        });
+        // Chain-ability of jQuery objects
+        return this;
+    };
+})(jQuery);
